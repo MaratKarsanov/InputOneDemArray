72c205b04f6b0387d499e510c94b9ddc03c9c8a2
marat <marat.karsanov@mail.ru>
2022-11-28 11:29:36 +0300

    Ввод элементов массива в строку

--- Program.cs
+++ Program.cs
@@ -10,13 +10,14 @@ namespace InputOneDirArray
     {
         static void Main(string[] args)
         {
-            //ввод элементов массива в столбик
+            //ввод элементов массива в строку
             int n = Convert.ToInt32(Console.ReadLine());
             int[] mas = new int[n];
-
+            string temp = Console.ReadLine();
+            string[] tempMass = temp.Split();
             for (var i = 0; i < n; i++)
             {
-                mas[i] = Convert.ToInt32(Console.ReadLine());
+                mas[i] = Convert.ToInt32(tempMass[i]);
             }
 
             foreach (var item in mas)
